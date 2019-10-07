d1bba61e130761c8bff8950b2cd59df7186a1c2e
webb <34105598+w3bb@users.noreply.github.com>
2019-10-07 09:35:09 -0400

    Also check for an optional discord.token file.

--- voicecreate.py
+++ voicecreate.py
@@ -14,7 +14,15 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix=".")
 bot.remove_command("help")
-DISCORD_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+try:
+    DISCORD_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+except:
+    try:
+        with open('discord.token', 'r') as tokeninput:
+            DISCORD_TOKEN=tokeninput.read().replace('\n', '')
+    except:
+        print("We could not find your bot's token in either the environment variable DISCORD_BOT_TOKEN or in the discord.token file.")
+        exit()
 
 initial_extensions = ['cogs.voice']
 
